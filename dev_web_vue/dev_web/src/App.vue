214db9037b9972775e23cbff45e028b587f85f6e
ada <106291993+boccar@users.noreply.github.com>
2023-06-22 21:21:32 -0300

    localstorage

--- dev_web_vue/dev_web/src/App.vue
+++ dev_web_vue/dev_web/src/App.vue
@@ -2,15 +2,17 @@
   <div class="container">
     <h1>Minhas Tarefas</h1>
     <div class="add-task">
-      <input type="text" v-model="newTask.title" placeholder="Digite o nome da tarefa...">
-      <textarea v-model="newTask.description" placeholder="Digite a descrição da tarefa..."></textarea>
-      <input type="date" v-model="newTask.date">
-      <input type="time" v-model="newTask.time">
-      <select v-model="newTask.category">
-        <option disabled value="">Selecione a categoria</option>
-        <option v-for="(category, index) in categories" :value="category" :key="index">{{ category }}</option>
-      </select>
-      <button @click="addTask">Adicionar</button>
+      <form>
+        <input type="text" v-model="newTask.title" placeholder="Digite o nome da tarefa...">
+        <textarea v-model="newTask.description" placeholder="Digite a descrição da tarefa..."></textarea>
+        <input type="date" v-model="newTask.date">
+        <input type="time" v-model="newTask.time">
+        <select v-model="newTask.category">
+          <option disabled value="">Selecione a categoria</option>
+          <option v-for="(category, index) in categories" :value="category" :key="index">{{ category }}</option>
+        </select>
+        <button @click="addTask">Adicionar</button>
+      </form>
     </div>
     <ul>
       <li v-for="(task, index) in tasks" :key="index">
@@ -43,17 +45,35 @@ export default {
     addTask() {
       if (this.newTask.title.trim() !== "") {
         this.tasks.push({ ...this.newTask, completed: false });
-        this.newTask.title = "";
-        this.newTask.description = "";
-        this.newTask.date = "";
-        this.newTask.time = "";
-        this.newTask.category = "";
+        this.saveTasksToLocalStorage(); 
+        this.resetNewTask();
       }
     },
     removeTask(index) {
       this.tasks.splice(index, 1);
+      this.saveTasksToLocalStorage();
+    },
+    saveTasksToLocalStorage() {
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    },
+    getTasksFromLocalStorage() {
+      const tasks = localStorage.getItem("tasks");
+      this.tasks = tasks ? JSON.parse(tasks) : [];
+    },
+    resetNewTask() {
+      this.newTask = {
+        title: "",
+        description: "",
+        date: "",
+        time: "",
+        category: ""
+      };
     }
+  },
+  mounted() {
+    this.getTasksFromLocalStorage();
   }
-};
+}
 </script>
+
 <style src="./style.css" scoped></style>
